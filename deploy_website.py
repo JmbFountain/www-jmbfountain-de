4e6230c6a77e69b99d38a4dbfd102ea78472356c
JmbFountain <jmb@smx.de>
2021-04-13 14:22:50 +0200

    added deployment script

---
+++ deploy_website.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import os
+
+# Get path of Project directory
+repoPath = os.path.dirname(__file__)
+print('Path to project:' , repoPath)
+sourceSitePath = repoPath + '/_site/'
+webroot = '/var/www/html/'
+copyJob = "cp -r " + sourceSitePath + "* " + webroot
+subprocess.Popen(copyJob)
