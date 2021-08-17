cc180a61863ad94897a88ad3293c5a4f1aefb93a
JmbFountain <jmb@smx.de>
2021-08-17 16:04:34 +0200

    added dockerfile

--- src/deploy_website.py
+++ src/deploy_website.py
@@ -4,7 +4,7 @@ import os
 # Get path of Project directory
 repoPath = os.path.dirname(__file__)
 print('Path to project:' , repoPath)
-sourceSitePath = repoPath + '/_site/'
+sourceSitePath = repoPath + '/src/_site/'
 webroot = '/var/www/html/'
 copyJob = "cp -r " + sourceSitePath + "* " + webroot
 subprocess.Popen(copyJob)
